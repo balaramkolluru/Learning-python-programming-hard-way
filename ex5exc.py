9002fb6c7092e8cab42a23706ad037c80a495889
balaramkolluru <38092705+balaramkolluru@users.noreply.github.com>
2021-06-10 19:37:37 +0530

    Convert pound to kg and inches to centimeters

---
+++ ex5exc.py
@@ -0,0 +1,9 @@
+# Convert inches to centimeters.
+inches = 19
+centimeters = inches * 2.54
+print "%r inches equals %r centimeters." % (inches, centimeters)
+
+# convert pouns to kilograms
+pounds = 180
+kilos = pounds * 0.45359237
+print "%r pounds equals %r kilos." % (pounds, kilos)
