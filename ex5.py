c3d823b6748ef00ce23309fb75f7f2164f317b58
balaramkolluru <38092705+balaramkolluru@users.noreply.github.com>
2021-06-10 19:36:26 +0530

    Strings with variables embedded

---
+++ ex5.py
@@ -0,0 +1,16 @@
+name = 'Ram'
+age = 30
+height = 78 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'white'
+hair_color = 'Brown'
+
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy")
+print(f"He's got {eyes} eyes and {hair_color} hair.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
